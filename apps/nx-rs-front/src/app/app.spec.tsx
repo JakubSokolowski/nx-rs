6918d23bf5180afa23401ae50e7e3c0edd64a062
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2022-02-01 19:13:17 +0100

    Mock component with wasm imports in app test

--- apps/nx-rs-front/src/app/app.spec.tsx
+++ apps/nx-rs-front/src/app/app.spec.tsx
@@ -2,6 +2,14 @@ import { render } from '@testing-library/react';
 
 import App from './app';
 
+// Workaround for "Jest encountered an unexpected token" when testing
+// component that has some child that imports some wasm module
+// This could probably be solved with some jest config option like moduleNameMapper
+// but mocking whole component works also. GameOfLife import is lazy so the factory
+// needs to return a function or class, instead of usual component:
+// { GameOfLife: () => Game of Life was here' } <- will not work
+jest.mock('../components/game-of-life', () => (() => 'Game of Life was here'));
+
 describe('App', () => {
   it('should render successfully', () => {
     const { baseElement } = render(<App />);
@@ -9,9 +17,9 @@ describe('App', () => {
     expect(baseElement).toBeTruthy();
   });
 
-  it('should have a greeting as the title', () => {
+  it('should display game of life', () => {
     const { getByText } = render(<App />);
 
-    expect(getByText('Welcome to nx-rs-front!')).toBeTruthy();
+    expect(getByText('Game of Life was here')).toBeTruthy();
   });
 });
