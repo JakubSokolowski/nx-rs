e166d68f4943c7a3ab30ce18573fff38aa4997c4
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2022-02-01 18:19:27 +0100

    Add rust deps to pipeline and fix e2e #7

--- apps/nx-rs-front-e2e/src/integration/app.spec.ts
+++ apps/nx-rs-front-e2e/src/integration/app.spec.ts
@@ -1,13 +1,7 @@
-import { getGreeting } from '../support/app.po';
-
 describe('nx-rs-front', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to nx-rs-front!');
+  it('should display container with game of life', () => {
+    cy.getByDataTest('game-of-life-title').contains("Game of Life");
   });
 });
