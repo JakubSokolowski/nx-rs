38603abcba89a88d96637fd2176a3fc05aed93a3
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2022-02-04 21:06:44 +0100

    Add controls #10

--- apps/nx-rs-front/src/components/game-of-life.tsx
+++ apps/nx-rs-front/src/components/game-of-life.tsx
@@ -1,101 +1,252 @@
-import { FC, useCallback, useEffect, useRef } from 'react';
+import { FC, MouseEventHandler, useEffect, useRef, useState, MouseEvent } from 'react';
 import { Cell, memory, Universe } from '@nx-rs/lib-rs';
 
-const CELL_SIZE_PX = 1;
+const CELL_SIZE_PX = 10;
+
+const GRID_COLOR = "#CCCCCC";
+const DEAD_COLOR = "#000000";
+const ALIVE_COLOR = "#FFFFFF";
+const HEIGHT = 50;
+const WIDTH = 100;
+
 
 const GameOfLife: FC = () => {
-  const universe = Universe.new(500, 500);
-  const width = universe.width();
-  const height = universe.height();
+  const universe = useRef(Universe.new(WIDTH, HEIGHT)) ;
+  const width = universe.current.width();
+  const height = universe.current.height();
   const canvasRef = useRef(null);
+  const numFramesToWait = useRef(2);
+  const [animationId, setAnimationId] = useState<number | null>(null);
 
-  const drawGrid = useCallback(
-    (ctx: CanvasRenderingContext2D) => {
-      ctx.beginPath();
-      ctx.strokeStyle = '#000000';
+  const drawGrid = (ctx: CanvasRenderingContext2D) => {
+    ctx.beginPath();
+    ctx.strokeStyle = GRID_COLOR;
 
-      // Vertical lines.
-      for (let i = 0; i <= width; i++) {
-        ctx.moveTo(i * (CELL_SIZE_PX + 1) + 1, 0);
-        ctx.lineTo(i * (CELL_SIZE_PX + 1) + 1, (CELL_SIZE_PX + 1) * height + 1);
+    // Vertical lines.
+    for (let i = 0; i <= width; i++) {
+      ctx.moveTo(i * (CELL_SIZE_PX + 1) + 1, 0);
+      ctx.lineTo(i * (CELL_SIZE_PX + 1) + 1, (CELL_SIZE_PX + 1) * height + 1);
+    }
+
+    // Horizontal lines.
+    for (let j = 0; j <= height; j++) {
+      ctx.moveTo(0, j * (CELL_SIZE_PX + 1) + 1);
+      ctx.lineTo((CELL_SIZE_PX + 1) * width + 1, j * (CELL_SIZE_PX + 1) + 1);
+    }
+
+    ctx.stroke();
+  };
+
+  const getIndex = (row: number, column: number) =>  {
+    return row * width + column;
+  };
+
+  const bitIsSet = (n: number, arr: Uint8Array) => {
+    const byte = Math.floor(n / 8);
+    const mask = 1 << (n % 8);
+    return (arr[byte] & mask) === mask;
+  };
+
+  const drawCells = (ctx: CanvasRenderingContext2D) => {
+    const cellsPtr = universe.current.cells();
+    const cells = new Uint8Array(memory.buffer, cellsPtr, width * height / 8);
+    ctx.beginPath();
+
+    for (let row = 0; row < height; row++) {
+      for (let col = 0; col < width; col++) {
+        const idx = getIndex(row, col);
+
+        ctx.fillStyle = bitIsSet(idx, cells) ? DEAD_COLOR : ALIVE_COLOR;
+
+        ctx.fillRect(
+          col * (CELL_SIZE_PX + 1) + 1,
+          row * (CELL_SIZE_PX + 1) + 1,
+          CELL_SIZE_PX,
+          CELL_SIZE_PX
+        );
       }
+    }
 
-      // Horizontal lines.
-      for (let j = 0; j <= height; j++) {
-        ctx.moveTo(0, j * (CELL_SIZE_PX + 1) + 1);
-        ctx.lineTo((CELL_SIZE_PX + 1) * width + 1, j * (CELL_SIZE_PX + 1) + 1);
-      }
+    ctx.stroke();
+  };
 
-      ctx.stroke();
-    },
-    [height, width]
-  );
+  const isPaused = () => {
+    return animationId === null;
+  };
 
-  const getIndex = useCallback(
-    (row: number, column: number): number => {
-      return row * width + column;
-    },
-    [width]
-  );
-
-  const drawCells = useCallback(
-    (ctx: CanvasRenderingContext2D) => {
-      const cellsPtr = universe.cells();
-      const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
-
-      ctx.beginPath();
-
-      for (let row = 0; row < height; row++) {
-        for (let col = 0; col < width; col++) {
-          const idx = getIndex(row, col);
-
-          ctx.fillStyle = cells[idx] === Cell.Dead ? '#FFFFFF' : '#000000';
-
-          ctx.fillRect(
-            col * (CELL_SIZE_PX + 1) + 1,
-            row * (CELL_SIZE_PX + 1) + 1,
-            CELL_SIZE_PX,
-            CELL_SIZE_PX
-          );
-        }
-      }
-
-      ctx.stroke();
-    },
-    [getIndex, height, universe, width]
-  );
-
-  useEffect(() => {
+  const renderLoop = () => {
     const canvas: any = canvasRef.current;
     const context: CanvasRenderingContext2D = canvas.getContext('2d');
     let frameCount = 0;
     let animationFrameId: any;
 
-    const draw = (ctx: any, frameCount: any) => {
-      universe.tick();
+    const draw = (ctx: CanvasRenderingContext2D, frameCount: number) => {
+      if(frameCount % numFramesToWait.current === 0) {
+        universe.current.tick();
+      }
       drawGrid(ctx);
       drawCells(ctx);
     };
 
-    const render = () => {
+    frameCount++;
+    draw(context, frameCount);
+
+    const drawFrame = () => {
       frameCount++;
       draw(context, frameCount);
-      setTimeout(() => {
-        animationFrameId = window.requestAnimationFrame(render);
-      }, 10);
+      animationFrameId = window.requestAnimationFrame(drawFrame);
+      setAnimationId(animationFrameId);
     };
-    render();
 
-    return () => {
-      window.cancelAnimationFrame(animationFrameId);
-    };
+    drawFrame();
+  }
+
+  useEffect(() => {
+    const canvas: any = canvasRef.current;
+    const ctx: CanvasRenderingContext2D = canvas.getContext('2d');
+    drawGrid(ctx);
+    drawCells(ctx);
   });
+
+  const play = () => {
+    renderLoop();
+  }
+
+  const reset = () => {
+    const canvas: any = canvasRef.current;
+    const ctx: CanvasRenderingContext2D = canvas.getContext('2d');
+    universe.current.reset();
+    drawGrid(ctx);
+    drawCells(ctx);
+  }
+
+
+  const random = () => {
+    const canvas: any = canvasRef.current;
+    const ctx: CanvasRenderingContext2D = canvas.getContext('2d');
+    universe.current.random(0.1);
+    drawGrid(ctx);
+    drawCells(ctx);
+  }
+
+
+  const pause = () => {
+    if(animationId) {
+      cancelAnimationFrame(animationId);
+      setAnimationId(null);
+      const canvas: any = canvasRef.current;
+      const ctx: CanvasRenderingContext2D = canvas.getContext('2d');
+      drawGrid(ctx);
+      drawCells(ctx);
+    }
+  }
+
+  const getRowColumn = (event: MouseEvent<HTMLCanvasElement>) => {
+    const canvas: any = canvasRef.current;
+    const boundingRect = canvas.getBoundingClientRect();
+
+    const scaleX = canvas.width / boundingRect.width;
+    const scaleY = canvas.height / boundingRect.height;
+
+    const canvasLeft = (event.clientX - boundingRect.left) * scaleX;
+    const canvasTop = (event.clientY - boundingRect.top) * scaleY;
+
+    const row = Math.min(Math.floor(canvasTop / (CELL_SIZE_PX + 1)), height - 1);
+    const col = Math.min(Math.floor(canvasLeft / (CELL_SIZE_PX + 1)), width - 1);
+    return [row, col];
+  }
+
+  const handleToggleClick: MouseEventHandler<HTMLCanvasElement> = (event) => {
+    const canvas: any = canvasRef.current;
+    const ctx: CanvasRenderingContext2D = canvas.getContext('2d');
+    const [row, col] = getRowColumn(event);
+    universe.current.toggle_cell(row, col);
+    drawGrid(ctx);
+    drawCells(ctx);
+  }
+
+  const handleGliderClick: MouseEventHandler<HTMLCanvasElement> = (event) => {
+    const [row, col] = getRowColumn(event);
+    const canvas: any = canvasRef.current;
+    const ctx: CanvasRenderingContext2D = canvas.getContext('2d');
+    universe.current.glider(row, col);
+    drawGrid(ctx);
+    drawCells(ctx);
+  }
+
+  const handlePulsarClick: MouseEventHandler<HTMLCanvasElement> = (event) => {
+    const [row, col] = getRowColumn(event);
+    const canvas: any = canvasRef.current;
+    const ctx: CanvasRenderingContext2D = canvas.getContext('2d');
+    universe.current.pulsar(row, col);
+    drawGrid(ctx);
+    drawCells(ctx);
+  }
+
+
+  const handleCanvasClick: MouseEventHandler<HTMLCanvasElement> = (event) => {
+    if(event.ctrlKey) {
+      handleGliderClick(event);
+    } else if(event.shiftKey) {
+      handlePulsarClick(event);
+    } else {
+      handleToggleClick(event);
+    }
+  }
 
   return (
     <div style={{ padding: '10px', width: '100%' }}>
       <h4 data-test="game-of-life-title">Game of Life</h4>
+      <div style={{display: 'flex', flexDirection: 'row'}}>
+        <div>
+          Click on cell to toggle. Ctrl + Click to insert <a href={"https://conwaylife.com/wiki/Glider"}>Glider</a>,
+          Shift + click to insert <a href={"https://conwaylife.com/wiki/Pulsar"}>Pulsar</a>
+        </div>
+        <div style={{flexGrow: 1}}/>
+        <a href={"https://github.com/JakubSokolowski/nx-rs"}>
+          source code
+        </a>
+      </div>
+      <div style={{height: '10px'}}/>
+      <div style={{display: 'flex', flexDirection: 'row'}}>
+        <button
+          onClick={
+            () => {
+              if(isPaused()) {
+                play();
+              } else {
+                pause();
+              }
+            }
+          }
+        >
+          {isPaused() ? "Start" : "Stop"}
+        </button>
+        <div style={{ width: '10px' }}/>
+        <button onClick={() => reset()}>
+          {"Reset"}
+        </button>
+        <div style={{ width: '10px' }}/>
+        <button onClick={() => random()}>
+          {"Random"}
+        </button>
+        <div style={{ width: '10px' }}/>
+        <input
+          type={'range'}
+          min={2}
+          max={10}
+          value={numFramesToWait.current}
+          onChange={(event) => numFramesToWait.current = Number.parseInt(event.target.value)}
+        />
+      </div>
       <div style={{ height: '10px' }} />
-      <canvas style={{ width: '100%', height: '100%' }} ref={canvasRef} />
+      <canvas
+        onClick={handleCanvasClick}
+        style={{ width: '100%', height: '100%' }}
+        width={(CELL_SIZE_PX + 1) * width + 1}
+        height={(CELL_SIZE_PX + 1) * height + 1}
+        ref={canvasRef}
+      />
     </div>
   );
 };
--- libs/lib-rs/src/lib.rs
+++ libs/lib-rs/src/lib.rs
@@ -1,4 +1,7 @@
 use wasm_bindgen::prelude::*;
+extern crate fixedbitset;
+extern crate js_sys;
+use fixedbitset::FixedBitSet;
 
 mod utils;
 
@@ -6,143 +9,229 @@ mod utils;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-#[wasm_bindgen]
-extern "C" {
-  fn alert(s: &str);
-  #[wasm_bindgen(js_namespace = console)]
-  fn log(s: &str);
-}
 
 #[wasm_bindgen]
-pub fn lmao() {
-  log("Anything but JS");
+extern "C" {
+    fn alert(s: &str);
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
 }
+
 
 #[wasm_bindgen]
 #[repr(u8)]
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum Cell {
-  Dead = 0,
-  Alive = 1,
+    Dead = 0,
+    Alive = 1,
 }
 
 #[wasm_bindgen]
 pub struct Universe {
-  width: u32,
-  height: u32,
-  cells: Vec<Cell>,
+    width: u32,
+    height: u32,
+    cells: FixedBitSet,
 }
 
+#[allow(clippy::unused_unit)]
 #[wasm_bindgen]
 impl Universe {
-  pub fn width(&self) -> u32 {
-    self.width
-  }
-
-  pub fn height(&self) -> u32 {
-    self.height
-  }
-
-  pub fn cells(&self) -> *const Cell {
-    self.cells.as_ptr()
-  }
-
-  pub fn new(w: u32, h: u32) -> Universe {
-    let width = w;
-    let height = h;
-
-    let cells = (0..width * height)
-      .map(|i| {
-        if i % 26 == 0 || i & 17 == 0 {
-          Cell::Alive
-        } else {
-          Cell::Dead
-        }
-      })
-      .collect();
-
-    Universe {
-      width,
-      height,
-      cells,
+    pub fn width(&self) -> u32 {
+        self.width
     }
-  }
 
-  pub fn tick(&mut self) {
-    let mut next = self.cells.clone();
+    pub fn height(&self) -> u32 {
+        self.height
+    }
 
-    for row in 0..self.height {
-      for col in 0..self.width {
-        let idx = self.get_index(row, col);
+    pub fn size(&self) -> usize {
+        (self.height * self.width) as usize
+    }
+
+    pub fn cells(&self) -> *const u32 {
+        self.cells.as_slice().as_ptr()
+    }
+
+    pub fn new(w: u32, h: u32) -> Universe {
+        let width = w;
+        let height = h;
+
+        let size = (width * height) as usize;
+        let mut cells = FixedBitSet::with_capacity(size);
+
+        for i in 0..size {
+            cells.set(i, js_sys::Math::random() < 0.1);
+        }
+
+        Universe {
+            width,
+            height,
+            cells,
+        }
+    }
+
+    pub fn random(&mut self, probability: f64) {
+        let size = self.size();
+        let mut cells = FixedBitSet::with_capacity(size);
+        for i in 0..size {
+            cells.set(i, js_sys::Math::random() < probability);
+        }
+        self.cells = cells;
+    }
+
+    pub fn reset(&mut self) {
+        self.cells = FixedBitSet::with_capacity(self.size());
+    }
+
+    pub fn tick(&mut self) {
+        let mut next = self.cells.clone();
+
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let idx = self.get_index(row, col);
+                let cell = self.cells[idx];
+                let live_neighbors = self.live_neighbor_count(row, col);
+
+                next.set(
+                    idx,
+                    match (cell, live_neighbors) {
+                        (true, x) if x < 2 => false,
+                        (true, 2) | (true, 3) => true,
+                        (true, x) if x > 3 => false,
+                        (false, 3) => true,
+                        (otherwise, _) => otherwise,
+                    },
+                );
+            }
+        }
+
+        self.cells = next;
+    }
+
+    pub fn toggle_cell(&mut self, row: u32, column: u32) {
+        let idx = self.get_index(row, column);
         let cell = self.cells[idx];
-        let live_neighbors = self.live_neighbor_count(row, col);
-
-        let next_cell = match (cell, live_neighbors) {
-          // Rule 1: Any live cell with fewer than two live neighbours
-          // dies, as if caused by underpopulation.
-          (Cell::Alive, x) if x < 2 => Cell::Dead,
-          // Rule 2: Any live cell with two or three live neighbours
-          // lives on to the next generation.
-          (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
-          // Rule 3: Any live cell with more than three live
-          // neighbours dies, as if by overpopulation.
-          (Cell::Alive, x) if x > 3 => Cell::Dead,
-          // Rule 4: Any dead cell with exactly three live neighbours
-          // becomes a live cell, as if by reproduction.
-          (Cell::Dead, 3) => Cell::Alive,
-          // All other cells remain in the same state.
-          (otherwise, _) => otherwise,
-        };
-
-        next[idx] = next_cell;
-      }
+        self.cells.set(idx, !cell);
     }
 
-    self.cells = next;
-  }
-
-  fn get_index(&self, row: u32, column: u32) -> usize {
-    (row * self.width + column) as usize
-  }
-
-  fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
-    let mut count = 0;
-    for delta_row in [self.height - 1, 0, 1].iter().cloned() {
-      for delta_col in [self.width - 1, 0, 1].iter().cloned() {
-        if delta_row == 0 && delta_col == 0 {
-          continue;
+    pub fn glider(&mut self, row: u32, column: u32) {
+        if self.will_fit(row as i32, column as i32, 3, 3) {
+            self.cells.set(self.get_index(row + 1, column), true);
+            self.cells.set(self.get_index(row, column - 1), true);
+            self.cells.set(self.get_index(row - 1, column - 1), true);
+            self.cells.set(self.get_index(row - 1, column), true);
+            self.cells.set(self.get_index(row - 1, column + 1), true);
         }
-
-        let neighbor_row = (row + delta_row) % self.height;
-        let neighbor_col = (column + delta_col) % self.width;
-        let idx = self.get_index(neighbor_row, neighbor_col);
-        count += self.cells[idx] as u8;
-      }
     }
-    count
-  }
+
+    pub fn pulsar(&mut self, row: u32, column: u32) {
+        if self.will_fit(row as i32, column as i32, 14, 14) {
+            self.cells.set(self.get_index(row - 6, column - 4), true);
+            self.cells.set(self.get_index(row - 6, column - 3), true);
+            self.cells.set(self.get_index(row - 6, column - 2), true);
+            self.cells.set(self.get_index(row - 6, column + 2), true);
+            self.cells.set(self.get_index(row - 6, column + 3), true);
+            self.cells.set(self.get_index(row - 6, column + 4), true);
+
+            self.cells.set(self.get_index(row - 2, column - 6), true);
+            self.cells.set(self.get_index(row - 3, column - 6), true);
+            self.cells.set(self.get_index(row - 4, column - 6), true);
+            self.cells.set(self.get_index(row + 2, column - 6), true);
+            self.cells.set(self.get_index(row + 3, column - 6), true);
+            self.cells.set(self.get_index(row + 4, column - 6), true);
+
+            self.cells.set(self.get_index(row - 2, column + 6), true);
+            self.cells.set(self.get_index(row - 3, column + 6), true);
+            self.cells.set(self.get_index(row - 4, column + 6), true);
+            self.cells.set(self.get_index(row + 2, column + 6), true);
+            self.cells.set(self.get_index(row + 3, column + 6), true);
+            self.cells.set(self.get_index(row + 4, column + 6), true);
+
+            self.cells.set(self.get_index(row - 1, column - 2), true);
+            self.cells.set(self.get_index(row - 1, column - 3), true);
+            self.cells.set(self.get_index(row - 1, column - 4), true);
+            self.cells.set(self.get_index(row - 1, column + 2), true);
+            self.cells.set(self.get_index(row - 1, column + 3), true);
+            self.cells.set(self.get_index(row - 1, column + 4), true);
+
+            self.cells.set(self.get_index(row + 1, column - 2), true);
+            self.cells.set(self.get_index(row + 1, column - 3), true);
+            self.cells.set(self.get_index(row + 1, column - 4), true);
+            self.cells.set(self.get_index(row + 1, column + 2), true);
+            self.cells.set(self.get_index(row + 1, column + 3), true);
+            self.cells.set(self.get_index(row + 1, column + 4), true);
+
+            self.cells.set(self.get_index(row + 6, column - 4), true);
+            self.cells.set(self.get_index(row + 6, column - 3), true);
+            self.cells.set(self.get_index(row + 6, column - 2), true);
+            self.cells.set(self.get_index(row + 6, column + 2), true);
+            self.cells.set(self.get_index(row + 6, column + 3), true);
+            self.cells.set(self.get_index(row + 6, column + 4), true);
+
+            self.cells.set(self.get_index(row - 2, column - 1), true);
+            self.cells.set(self.get_index(row - 3, column - 1), true);
+            self.cells.set(self.get_index(row - 4, column - 1), true);
+            self.cells.set(self.get_index(row + 2, column - 1), true);
+            self.cells.set(self.get_index(row + 3, column - 1), true);
+            self.cells.set(self.get_index(row + 4, column - 1), true);
+
+            self.cells.set(self.get_index(row - 2, column + 1), true);
+            self.cells.set(self.get_index(row - 3, column + 1), true);
+            self.cells.set(self.get_index(row - 4, column + 1), true);
+            self.cells.set(self.get_index(row + 2, column + 1), true);
+            self.cells.set(self.get_index(row + 3, column + 1), true);
+            self.cells.set(self.get_index(row + 4, column + 1), true);
+        }
+    }
+
+    fn will_fit(&self, row: i32, column: i32, width: i32, height: i32) -> bool {
+        let w2 = width / 2 + 1;
+        let h2 = height / 2 + 1;
+        row - h2 > 0
+            && row + h2 < self.height as i32
+            && column - w2 > 0
+            && column + w2 < self.width as i32
+    }
+
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize
+    }
+
+    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0;
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+
+                let neighbor_row = (row + delta_row) % self.height;
+                let neighbor_col = (column + delta_col) % self.width;
+                let idx = self.get_index(neighbor_row, neighbor_col);
+                count += self.cells[idx] as u8;
+            }
+        }
+        count
+    }
 }
 
 impl Universe {
-  pub fn set_width(&mut self, width: u32) {
-    self.width = width;
-    self.cells = (0..width * self.height).map(|_i| Cell::Dead).collect();
-  }
-
-  pub fn get_cells(&self) -> &[Cell] {
-    &self.cells
-  }
-
-  pub fn set_height(&mut self, height: u32) {
-    self.height = height;
-    self.cells = (0..self.width * height).map(|_i| Cell::Dead).collect();
-  }
-
-  pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
-    for (row, col) in cells.iter().cloned() {
-      let idx = self.get_index(row, col);
-      self.cells[idx] = Cell::Alive;
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        let size = (width * self.height) as usize;
+        self.cells = FixedBitSet::with_capacity(size);
     }
-  }
-}
 
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        let size = (self.width * height) as usize;
+        self.cells = FixedBitSet::with_capacity(size);
+    }
+
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells.set(idx, true);
+        }
+    }
+}
